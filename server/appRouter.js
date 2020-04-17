0ec7ec00c7174d1daa47205514cd547531da0222
gedaersu <QINlove123>
2020-04-17 21:18:49 +0800

    后台路由

---
+++ server/appRouter.js
@@ -0,0 +1,32 @@
+//后台应用的路由器模块
+//引入express
+const express = require('express')
+const md5 = require('blueimp-md5')
+const models = require('./models')
+const UserModel = models.getModel('user')
+//得到路由器
+const router = express.Router()
+//注册n个路由
+  //注册路由
+router.post('/register',function (req, res) {
+// 1.获取请求参数数据(name，pwd，type)
+  const {name, pwd, type} = req.body
+// 2.处理数据
+  //2.1根据name查询数据库，检查是否已经存在
+  UserModel.findOne({name},function (err, user) {
+    if (user) {
+      res.send({code: 1, msg:'此用户名已存在'})
+    } else {
+      new UserModel({name, pwd: md5(pwd), type}).save(function (err, user) {
+        res.cookie('userid', user._id)
+        res.send({code: 0, data: {_id: user._id, name, type}})
+      })
+    }
+  })
+// 3.返回响应数据
+
+})
+  //登陆路由
+//向外暴露路由器
+module.exports = router
+//通过app使用上路由器(server中编写)
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 /*启动服务器应用的模块*/
 const express = require('express')
+const bodyParser = require('body-parser') //解析请求体
+const cookieParser = require('cookie-parser') //解析cookie
+const appRouter = require('./appRouter')
 const app = express()
-app.use('/',function (rep,res) {
+/*app.use('/',function (rep,res) {
   res.send('Hello Server!')
-})
+})*/
+app.use(cookieParser()) //解析cookie
+app.use(bodyParser.json()) //解析请求体(ajax请求：json数据格式)
+app.use(bodyParser.urlencoded({ extended: false })) //解析请求体(表单数据)
+app.use('/api', appRouter) //请求注册：/api/register
 app.listen('8888',function () {
   console.log('server start on port: 8888')
 })
