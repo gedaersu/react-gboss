f290e9b9ea929dd702a909aa620d02239efeb393
gedaersu <QINlove123>
2020-04-17 23:21:17 +0800

    配置api，添加"proxy": "http://localhost:4000"代理，解决基本跨域

--- server/appRouter.js
+++ server/appRouter.js
@@ -4,6 +4,7 @@ const express = require('express')
 const md5 = require('blueimp-md5')
 const models = require('./models')
 const UserModel = models.getModel('user')
+const filter = {pwd: 0}
 //得到路由器
 const router = express.Router()
 //注册n个路由
@@ -14,6 +15,7 @@ router.post('/register',function (req, res) {
 // 2.处理数据
   //2.1根据name查询数据库，检查是否已经存在
   UserModel.findOne({name},function (err, user) {
+// 3.返回响应数据
     if (user) {
       res.send({code: 1, msg:'此用户名已存在'})
     } else {
@@ -23,10 +25,20 @@ router.post('/register',function (req, res) {
       })
     }
   })
-// 3.返回响应数据
 
 })
   //登陆路由
+router.use('/login',function (req, res) {
+  const {name, pwd} = req.body
+  UserModel.findOne({name, pwd: md5(pwd)}, filter,function (err, user) {
+    if(!user){
+      res.send({code: 1, msg:'用户名或密码错误'})
+    } else {
+      res.cookie('userid', user._id)
+      res.send({code: 0, data: user})
+    }
+  })
+})
 //向外暴露路由器
 module.exports = router
 //通过app使用上路由器(server中编写)
---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+/*包含 n 个接口请求函数的模块
+每个函数返回的都是 promise 对象 */
+import ajax from './ajax'
+// 请求注册
+export const reqRegister = (user) => ajax('/register', user, 'POST')
+// 请求登陆
+export const reqLogin = (user) => ajax('/login', user, 'POST')
